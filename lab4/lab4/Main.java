f8dc7d9e2266e196450e3d860599e641f1b8612c
dima0808 <52035263+dima0808@users.noreply.github.com>
2023-02-08 18:42:02 +0100

    Add files via upload

--- lab4/lab4/Main.java
+++ lab4/lab4/Main.java
@@ -5,20 +5,28 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Створення об'єктів класу Student.
         Student First = new Student("Михайло", "Гордун", 21, "ІПСА", true);
         Student Second = new Student("Дмитро", "Шарій", 19, "ТЕФ", false);
         Student Third = new Student("Аліна", "Шарій", 18, "ЕФ", false);
         Student Fourth = new Student("Назар", "Лоленко", 17, "ФІОТ", true);
         Student Fifth = new Student("Олександра", "Зеленська", 18, "ФМВ", true);
 
+        // Створення масиву об'єктів класу Student.
         Student[] ObjArray = new Student[] {First, Second, Third, Fourth, Fifth};
 
+        // Сортуваня по алфавіту (прізвище ім'я).
         Arrays.sort(ObjArray, new Sorting.SortByAlphabet());
         System.out.println("\nСортування за алфавітом (зростання):");
         for (Student Classes : ObjArray) {
             System.out.println(Classes.lastName + " " + Classes.firstName);
         }
 
+        /* Сортуваня по віці (спадання).
+        Масив сортує масив по зростанню, але в циклі ітерація проходить навпаки,
+        тим самим виводячи елементи з кінця до початку.
+        */
         Arrays.sort(ObjArray, new Sorting.SortByAge());
         System.out.println("\nСортування за віком (спадання):");
         for (int i = ObjArray.length-1; i >= 0; i--) {
--- lab4/lab4/Student.java
+++ lab4/lab4/Student.java
@@ -2,17 +2,22 @@ package lab4;
 
 public class Student {
 
+    // Створення полів класу.
     public String firstName;
     public String lastName;
     public int age;
-    public String faculty;
-    public boolean hasStipend;
+    private String faculty;
+    private boolean hasStipend;
 
+    // Конструктор класу.
     public Student(String fn, String ln, int age, String fac, boolean wealth) {
         setValues(fn, ln, age, fac, wealth);
         getValues();
     }
 
+    /* Метод, який нічого не повертає.
+    Визначає поля класу.
+    */
     private void setValues(String fn, String ln, int age, String fac, boolean wealth) {
         this.firstName = fn;
         this.lastName = ln;
@@ -21,6 +26,9 @@ public class Student {
         this.hasStipend = wealth;
     }
 
+    /* Метод, який нічого не повертає.
+    Виводить значення полів.
+    */
     private void getValues() {
         System.out.println("Прізвище та ім'я: " + this.lastName + " " + this.firstName);
         System.out.println("Вік: " + this.age);
